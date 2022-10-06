d570dd5635002d4ea3b6eea517115f6ac5bfe1b9
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-10-06 21:08:41 +0100

    make 16 * 16 grid

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+    container = document.querySelector('.container')
+    for(let i = 0; i < 256; i++){
+        div = document.createElement('div')
+        div.classList = ('div')
+        container.append(div)
+    }
+})
