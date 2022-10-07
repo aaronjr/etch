ced311ad05a7cc1ca5a406c22b2814c0ae3e2a96
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-10-07 08:48:28 +0100

    size of drawing pag responsive to input

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 document.addEventListener('DOMContentLoaded', function(){
-    container = document.querySelector('.container')
-    for(let i = 0; i < 256; i++){
-        div = document.createElement('div')
-        div.classList = ('div')
-        container.append(div)
-    }
 
+    document.querySelector('.size').addEventListener('keyup', () => {
+        width = document.querySelector('.size')
+        size(width.value)
+    })
+
+    container = document.querySelector('.container')
+
+    function size(number = 16){
+        total = number * number
+        while(container.lastChild){
+            container.removeChild(container.lastChild)
+        }
+        container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+
+        for(let i = 0; i < total; i++){
+            div = document.createElement('div')
+            div.classList = ('div')
+            container.append(div)
+        }
+    }
+    
     divs = document.querySelectorAll('.div')
         divs.forEach((div)=>{
             div.addEventListener('mouseover', () => {
                 div.classList = ('color')
             })
         })
+
+    size()
 })
